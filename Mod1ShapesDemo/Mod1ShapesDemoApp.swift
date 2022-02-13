a5ef327bf4f2c7c78d25468352cffa4482618961
agholson <87455899+agholson@users.noreply.github.com>
2022-02-13 15:03:27 -0500

    Initial Commit

---
+++ Mod1ShapesDemo/Mod1ShapesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod1ShapesDemoApp.swift
+//  Mod1ShapesDemo
+//
+//  Created by Leone on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Mod1ShapesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
